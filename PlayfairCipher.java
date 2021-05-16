647b245712c5e272d39b2a05575e9f0917b510f2
AlbinaAhmeti <albinaahmeti6@gmail.com>
2021-05-16 22:23:08 +0200

    PlayfairCipher

--- PlayfairCipher.java
+++ PlayfairCipher.java
@@ -30,3 +30,46 @@ this.keyMatrix(matrix);
 
 }
 
+private String[][] cipherMatrix(String key)
+{
+String[][] playfairMatrix=new String[5][5];
+String keyString=key+"ABCDEFGHIKLMNOPQRSTUVWXYZ"; 
+
+for(int i=0;i<5;i++)
+for(int j=0;j<5;j++)
+
+for(int k = 0; k < keyString.length(); k++)  
+{  
+boolean repeat = false;  
+boolean used = false;  
+for(int i = 0; i < 5; i++)  
+{  
+for(int j = 0; j < 5; j++)  
+{  
+if(playfairMatrix[i][j].equals("" + keyString.charAt(k)))  
+{  
+repeat = true;  
+}  
+else if(playfairMatrix[i][j].equals("") && !repeat && !used)  
+{  
+playfairMatrix[i][j] = "" + keyString.charAt(k);  
+used = true;  
+}  
+}  
+}  
+}  
+return playfairMatrix;  
+}  
+
+private String cipher(String in)
+{
+length =(int) in.length()/2+in.length()%2;
+
+for(int i=0;i<(length-1);i++)
+{
+if(in.charAt(2 * i) == in.charAt(2 * i + 1))  
+{  
+in = new StringBuffer(in).insert(2 * i + 1, 'X').toString();  
+length = (int) in.length() / 2 + in.length() % 2;  
+}  
+}  
